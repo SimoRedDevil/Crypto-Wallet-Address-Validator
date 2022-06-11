b9c69358a92e3a4566e93f191f9f5b8e29612511
Mohamed El Youssfi <mohamedyoussfi20@gmail.com>
2022-06-11 23:30:29 +0100

    Project is done !

--- Crypto Wallet Address Validator/MainWindow.xaml.cs
+++ Crypto Wallet Address Validator/MainWindow.xaml.cs
@@ -12,17 +12,37 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+using System.Text.Json;
+using System.Threading;
 
 namespace Crypto_Wallet_Address_Validator
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    public class Result
+    {
+        public string address { get; set; }
+        public string currency { get; set; }
+        public string network { get; set; }
+        public bool isValid { get; set; }
+    }
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            ServicePointManager.Expect100Continue = true;
+
+            string[] Coins = new string[]
+            {
+                "BTC", "BCH", "BTG", "BTCP", "BTCZ", "DASH", "DGB", "DOGE", "ETH", "ETC", "ETZ", "HUSH",
+                "LTC", "XMR", "NMC", "NANO", "NEO", "XPM", "QTUM", "XRB", "XRP", "VTC", "ZEC", "ZCL", "ZEN"
+            };
+            ComboCoins.ItemsSource = Coins;
         }
 
         private void TextAddress_TextChanged(object sender, TextChangedEventArgs e)
@@ -37,7 +57,23 @@ namespace Crypto_Wallet_Address_Validator
 
         private void ButtonCheck_Click(object sender, RoutedEventArgs e)
         {
-
+            if (!string.IsNullOrEmpty(TextAddress.Text) && !string.IsNullOrEmpty(ComboCoins.Text))
+            {
+                string ResultJSONFormat = RapidAPI.ValidateAddress(TextAddress.Text, ComboCoins.Text);
+                Result CheckResult = JsonSerializer.Deserialize<Result>(ResultJSONFormat);
+                if (CheckResult.isValid == true)
+                {
+                    LabelResult.Foreground = new SolidColorBrush(Color.FromRgb(97, 196, 165));
+                    LabelResult.Content = "Wallet Address Syntax Looks Valid.";
+                    LabelResult.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    LabelResult.Foreground = new SolidColorBrush(Color.FromRgb(238, 82, 83));
+                    LabelResult.Content = "Wallet Address Syntax Looks Invalid.";
+                    LabelResult.Visibility = Visibility.Visible;
+                }
+            }
         }
     }
 }
--- Crypto Wallet Address Validator/RapidAPI.cs
+++ Crypto Wallet Address Validator/RapidAPI.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net;
+using System.Windows;
 
 namespace Crypto_Wallet_Address_Validator
 {
@@ -12,23 +13,31 @@ namespace Crypto_Wallet_Address_Validator
     {
         public static string ValidateAddress(string address, string coin)
         {
-			var client = new HttpClient();
-			var request = new HttpRequestMessage
-			{
-				Method = HttpMethod.Get,
-				RequestUri = new Uri($"https://crypto-wallet-address-validator.p.rapidapi.com/validate/{address}?currency={coin}&network=prod"),
-				Headers =
+			try
+            {
+				var client = new HttpClient();
+				var request = new HttpRequestMessage
+				{
+					Method = HttpMethod.Get,
+					RequestUri = new Uri($"https://crypto-wallet-address-validator.p.rapidapi.com/validate/{address}?currency={coin}&network=prod"),
+					Headers =
 	{
 		{ "X-RapidAPI-Key", "c111b8ee68msh27105aa2fe988ebp1f7bd1jsn0a971cf084d2" },
 		{ "X-RapidAPI-Host", "crypto-wallet-address-validator.p.rapidapi.com" },
 	},
-			};
-			using (var response = client.SendAsync(request).Result)
-			{
-				response.EnsureSuccessStatusCode();
-				var body = response.Content.ReadAsStringAsync().Result;
-				return body;
+				};
+				using (var response = client.SendAsync(request).Result)
+				{
+					response.EnsureSuccessStatusCode();
+					var body = response.Content.ReadAsStringAsync().Result;
+					return body;
+				}
 			}
+			catch(Exception ex)
+            {
+				MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+			return null;
 		}
     }
 }
